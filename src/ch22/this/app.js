afa9442c852a3b9da113e2afac2b72080d185ee6
Lee-sung-il <wj132612@naver.com>
2025-03-12 13:40:49 +0900

    this

---
+++ src/ch22/this/app.js
@@ -0,0 +1,16 @@
+const person = {
+    firstName: 'Viggo',
+    lastName: 'Mortensen',
+    fullName: function () {
+        return `${this.firstName} ${this.lastName}`;
+    },
+    shoutName: function () {
+        setTimeout(() => {
+            console.log(this);
+            console.log(this.fullName())
+        }, 3000)
+    }
+};
+
+console.log(person.shoutName());
+
