a298eb1e8f44569e17ca5919405774825085302e
Lee-sung-il <wj132612@naver.com>
2025-03-14 18:30:46 +0900

    delte

---
+++ src/ch25/Delegation/app.js
@@ -0,0 +1,34 @@
+/*const list = document.querySelectorAll('li');
+
+for (let li of list) {
+  li.addEventListener('click', () => {
+    li.remove();
+  });
+}*/
+
+const tweetForm = document.querySelector('#tweetForm');
+const tweetsContainer = document.querySelector('#tweets');
+tweetForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // const usernameInput = document.querySelectorAll('input')[0];
+  // const tweetInput = document.querySelectorAll('input')[1];
+  console.log(this);
+  const usernameInput = tweetForm.elements.username;
+  const tweetInput = e.target.elements.tweet;
+  addTweet(usernameInput.value, tweetInput.value);
+  usernameInput.value = '';
+  tweetInput.value = '';
+});
+
+const addTweet = (username, tweet) => {
+  const newTweet = document.createElement('li');
+  const bTag = document.createElement('b');
+  bTag.append(username);
+  newTweet.append(bTag);
+  newTweet.append(`-${tweet}`);
+  tweetsContainer.append(newTweet);
+};
+
+tweetsContainer.addEventListener('click', (e) => {
+  e.target.nodeName === 'LI' && e.target.remove();
+});
