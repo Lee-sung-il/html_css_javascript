1f8469133ef2b7d40f7eb0997f97e9bde95bf4af
Lee-sung-il <wj132612@naver.com>
2025-03-14 11:47:49 +0900

    이벤트 기본" ; "

---
+++ src/ch25/EventBasics/app.js
@@ -0,0 +1,36 @@
+const btn = document.querySelector('#v2');
+btn.onclick = () => {
+  console.log('YOU CLICKED ME!');
+  console.log('I HOPE IT WORKED!!!');
+};
+
+function scream() {
+  console.log('AAAAAHHHHH');
+  console.log('STOP TOUCHING ME!');
+}
+
+btn.onmouseenter = scream;
+
+document.querySelector('h1').onclick = () => {
+  alert('you clicked the h1!');
+};
+
+const btn3 = document.querySelector('#v3');
+btn3.addEventListener('click', () => {
+  alert('Clicked');
+});
+
+function twist() {
+  console.log('TWIST!');
+}
+
+function shout() {
+  console.log('SHOUT!');
+}
+
+const tasButton = document.querySelector('#tas');
+/*tasButton.onclick = twist;
+tasButton.onclick = shout;*/
+
+tasButton.addEventListener('click', twist);
+tasButton.addEventListener('click', shout);
