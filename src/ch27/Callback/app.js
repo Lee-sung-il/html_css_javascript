b1b70a581177a36fd3ff95eafe02192995920a89
Lee-sung-il <wj132612@naver.com>
2025-03-16 22:40:36 +0900

    callback

---
+++ src/ch27/Callback/app.js
@@ -0,0 +1,14 @@
+/*
+const multiply = (x, y) => x * y;
+const square = (x) => multiply(x, x);
+const isRightTriangle = (a, b, c) => {
+  return square(a) + square(b) === square(c);
+};
+console.log(isRightTriangle(3, 4, 5));
+*/
+
+console.log('Sending request to server!');
+setTimeout(() => {
+  console.log('Here is your data from the server...');
+}, 3000);
+console.log('I AM AT THE END OF THE FILE!');
