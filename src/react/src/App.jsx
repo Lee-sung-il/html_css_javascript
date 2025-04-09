62d41242e617d9c41833f4a62b5d6a9b6645432f
Lee-sung-il <wj132612@naver.com>
2025-04-09 11:45:10 +0900

    ✨ React 생성
    포켓몬 카드 모양 데모

---
+++ src/react/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import Greeter from './Greeter.jsx';
+import Dog from './Dog.jsx';
+import LoginForm from './LoginForm.jsx';
+import Die from './Die.jsx';
+import DiceRoll from './DiceRoll.jsx';
+import RandomPokemon from './RandomPokemon.jsx';
+
+function App() {
+  return (
+    <div className="App  ">
+      {/*<LoginForm/>*/}
+      {/*<Greeter/>*/}
+      {/*<Dog/>*/}
+      {/* <DiceRoll />
+      <DiceRoll />*/}
+      <RandomPokemon />
+    </div>
+  );
+}
+
+export default App;
